226caeafdaf3bec0e2d57175f2cd97d6c966fe39
bbarron6095 <34726500+bbarron6095@users.noreply.github.com>
2021-01-14 16:38:14 -0800

    Add files via upload

---
+++ before_after.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Featured</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,300&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@700&display=swap" rel="stylesheet"> 
+        <link rel="preconnect" href="https://fonts.gstatic.com"> 
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet"> 
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
+        
+        
+        
+    </head>
+    <body>
+        
+        <div class="fixed-top">
+            <div class="call">
+                 <h2 id="call-us">Contact Us Today !</h2>
+                 <a href="tel:775-386-8471" id="phone">775-386-8471</a>
+                 <a href="mailto:silversummitlandscaping@gmail.com" id="email-us"> silversummitlandscaping@gmail.com</a>
+            </div>
+            <nav>
+                    <a href="#" id="bar" onclick="show()"><i class="fas fa-bars"></i></a>
+                    <ul id="menu-items">
+                        <a class="nav" href="index.php#home"><li>Home</li></a>
+                        <a class="nav"href="index.php#about"><li>About</li></a>
+                        <a class="nav" href="index.php#services"><li>Services</li></a>
+                        <a class="nav" href="gallery.php"><li>Gallery</li></a>
+                        <a class="nav" href="index.php#reviews"><li>Reviews</li></a>
+                        <a class="nav" href="index.php#contact"><li>Contact Us</li></a>
+                    </ul>
+                
+               
+                </nav>
+            </div>
+
+            <div class="nav2">
+                <ul id="menu-items2">
+                        <a class="nav2" href="index.php#home"><li id="two">Home</li></a>
+                        <a class="nav2"href="#about"><li id="two">About</li></a>
+                        <a class="nav2" href="index.php#services"><li id="two">Services</li></a>
+                        <a class="nav2" href="gallery.php"><li id="two">Gallery</li></a>
+                        <a class="nav2" href="index.php#reviews"><li id="two">Reviews</li></a>
+                        <a class="nav2" href="index.php#contact"><li id="two">Contact Us</li></a>
+                    </ul>
+                </div>
+        </div>
+        <div class="go-back">
+                     <a class="btn btn-primary" id="back-btn" href="gallery.php">Go Back</a>
+          </div>
+
+       <div class="container">
+      
+                 <div class="gallery1">
+                    <h1 id="gallery1-title"> Heated Concrete</h1>
+                    <img class="gallery1-img" src="./images/heated concrete4.jpg" alt="heated concrete picture" >
+                    <br>
+                    <a class="btn btn-success" id="gallery1-link" href="heated_concrete.php">View More</a>
+                 </div> 
+                 
+                 <div class="gallery1">
+                    <h1 id="gallery1-title"> Before and Afters</h1>
+                    <img class="gallery1-img" src="./images/beforeandafter.png" alt="before and after picture" >
+                    <a class="btn btn-success" id="gallery1-link" href="compare.php">View More</a>
+                 </div>
+                 <div class="gallery1">
+                    <h1 id="gallery1-title"> Remodels</h1>
+                    <img class="gallery1-img" src="./images/bathroom1.jpg" alt="Bathroom remodel picture" >
+                    <a class="btn btn-success" id="gallery1-link" href="remodel.php">View More</a>
+                 </div>
+                 
+          </div>
+         
+    <footer class="footer-distributed">
+
+      <div class="footer-left">
+  
+        <h3>Silver Summit Landscaping LLC.</h3>
+
+        <p class="footer-links">
+          <a href="index.php#home">Home</a>
+          |
+          <a href="index.php#about">About</a>
+          |
+          <a href="index.php#services">Services</a>
+          |
+          <a href="gallery.php">Gallery</a>
+          |
+          <a href="index.php#reviews">Reviews</a>
+          |
+          <a href="index.php#contact">Contact</a>
+        </p>
+
+        <p class="footer-company-name">© 2021 Silver Summit Landscaping LLC.</p>
+      </div>
+
+      <div class="footer-center">
+        <div>
+          <i class="fa fa-map-marker"></i>
+            <p> Proudly serving Reno and Surrounding Areas!</p>
+        </div>
+
+        <div id="num">
+          <i class="fa fa-phone" id="ph"></i>
+          <a href="tel:775-386-8471" id="our-phone" >775-386-8471</a>
+        </div>
+        <div id="em">
+          <i class="fa fa-envelope" id="envelope"></i>
+          <p><a href="mailto:silversummitlandscaping@gmail.com" id="en">silversummitlandscaping@gmail.com</a></p>
+        </div>
+        <div>
+          <i class="fab fa-google"></i>
+          <p><a href="https://g.page/SilverSummitLandscapingLLC/review?gm" id="google"> Leave us a review on Google</a></p>
+        </div>
+        <div class="footer-icons">
+          <a href="#"><i class="fab fa-facebook fa-2x" id="social-icon1"></i></a>
+          <a href="https://www.yelp.com/biz/sierra-summit-cleaning-and-landscape-reno-9"><i class="fab fa-yelp fa-2x" id="social-icon2"></i></a>
+          <a href="https://www.instagram.com/silver_summit_llc/"><i class="fab fa-instagram fa-2x" id="social-icon3"></i></a>  
+        </div>
+      </div>
+      <div class="footer-right">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d196868.99829069863!2d-119.99126872797443!3d39.55784655377313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x809940ae9292a09d%3A0x40c5c5ce7438f787!2sReno%2C%20NV!5e0!3m2!1sen!2sus!4v1610399224268!5m2!1sen!2sus"  id="map" width="400" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+    </div>
+</footer>
+
+          
+<script>
+        function show() {
+        var items = document.getElementById("menu-items2");
+        if (items.style.display === "none") {
+            items.style.display = "flex";
+        } else {
+            items.style.display = "none";
+        }
+        }
+ </script> 
+      
+        
+        <script src="" async defer></script>
+    </body>
+</html>
